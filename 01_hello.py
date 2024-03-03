17445c7d58ce4610ed138ad8f1900f715602e16a
Doug Duncan <duncan.doug@gmail.com>
2024-03-03 12:37:31 -0600

    pre-solution

---
+++ 01_hello.py
@@ -0,0 +1,8 @@
+# When first run, print 'Hello, world!'
+# Then, ask the user to enter their name --> "What is your name?"
+# Lastly, print 'Hello, {name}'
+
+# Assertions - check that input is valid
+print("Hello, world!")
+name = input("What is your name? ")
+print(f'Hello, {name}')
