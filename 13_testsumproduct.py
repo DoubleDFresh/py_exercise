c1395fb200d28cdbc761107e5eb20490a3e8060b
Doug Duncan <duncan.doug@gmail.com>
2024-03-07 20:03:14 -0600

    test file for sumproduct.py

--- 13_testsumproduct.py
+++ 13_testsumproduct.py
@@ -1,8 +1,8 @@
-"""create 10,000 random numbers and sum and product
+    """create 10,000 random numbers and sum and product
 
 
-"""
-# the argument is passed to the function and assigned as the value to the parameter"""calculateSum returns the sum of a list argument, calculateProduct returns the product of the list argument
+    """
+    # the argument is passed to the function and assigned as the value to the parameter"""calculateSum returns the sum of a list argument, calculateProduct returns the product of the list argument
 
 import random, sumproduct
 
